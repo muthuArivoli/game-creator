41c0a7f066a7576f4061e9af148a3909b2b8153d
Alexander Uzochukwu <aou@duke.edu>
2020-04-10 22:52:05 -0400

    Added resources package and default css file

--- src/ooga/game_view/GameGuiController.java
+++ src/ooga/game_view/GameGuiController.java
@@ -1,9 +1,35 @@
 package ooga.game_view;
 
+import java.io.IOException;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class GameGuiController extends Application {
+
+  //private static final String STYLESHEET = "slogo/resources/styleSheets/default.css";
+  //private static final String IMAGE_DIRECTORY = "src/slogo/resources/images";
+  //private static final String LOGO_DIRECTORY = "data/examples";
+  //private static final String RESOURCES_PACKAGE = "slogo.resources.languages.GUI.";
+  private static final String IMAGE_FILE_EXTENSIONS = "*.png,*.jpg";
+  private static final double FRAMES_PER_SECOND = 30;
+  private static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
+  private static final double SCENE_WIDTH = 1280;
+  private static final double SCENE_HEIGHT = 720;
+  private static final Color ALL_COLOR = Color.WHITE;
+
+  private BorderPane root;
+  private Stage myStage;
+  private Timeline animation;
 
   /**
    * Empty Constructor needed to run the application due to Application requirements
@@ -25,7 +51,38 @@ public class GameGuiController extends Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception {
+    primaryStage.setTitle("Game Engine");
+    myStage = primaryStage;
+    startAnimationLoop();
+    setBorderPane();
+    Scene scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
+    //scene.getStylesheets().add(STYLESHEET);
+    myStage.setScene(scene);
+    myStage.show();
+  }
+
+  private void setBorderPane() {
+    root = new BorderPane();
+    root.setBackground(new Background(new BackgroundFill(ALL_COLOR, CornerRadii.EMPTY, Insets.EMPTY)));
+    root.setMaxWidth(SCENE_WIDTH);
+    root.setMaxHeight(SCENE_HEIGHT);
+  }
+
+  private void startAnimationLoop() {
+    KeyFrame frame = new KeyFrame(Duration.seconds(SECOND_DELAY), e -> {
+      try {
+        step(); }
+      catch (IOException ex) {
+        System.out.println("Help Text File Not Found."); } });
+    animation = new Timeline();
+    animation.setCycleCount(Timeline.INDEFINITE);
+    animation.getKeyFrames().add(frame);
+    animation.play();
+  }
+
+  private void step() throws IOException {
 
   }
+
 
 }
