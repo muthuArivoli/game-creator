2f1c62d1e04fb0ea72b7915f5b4f4e4e53181ea3
Alexander Uzochukwu <aou@duke.edu>
2020-04-11 01:41:24 -0400

    Added game board

--- src/ooga/game_view/GameGuiController.java
+++ src/ooga/game_view/GameGuiController.java
@@ -1,35 +1,45 @@
 package ooga.game_view;
 
 import java.io.IOException;
+import java.util.ResourceBundle;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import java.io.File;
 
 public class GameGuiController extends Application {
 
-  //private static final String STYLESHEET = "slogo/resources/styleSheets/default.css";
-  //private static final String IMAGE_DIRECTORY = "src/slogo/resources/images";
-  //private static final String LOGO_DIRECTORY = "data/examples";
-  //private static final String RESOURCES_PACKAGE = "slogo.resources.languages.GUI.";
-  private static final String IMAGE_FILE_EXTENSIONS = "*.png,*.jpg";
+  private static final String STYLESHEET = "ooga/resources/styleSheets/default.css";
+  private static final String PIECES_DIRECTORY = "src/ooga/resources/images/pieces";
+  private static final String LANGUAGES_PACKAGE = "ooga.resources.languages.";
+  private static final String GAME_DIRECTORY = "ooga.resources.gameFiles";
+  private static final String GAME_FILE_EXTENSIONS = "*.xml,*.json,*.csv";
   private static final double FRAMES_PER_SECOND = 30;
   private static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
   private static final double SCENE_WIDTH = 1280;
   private static final double SCENE_HEIGHT = 720;
   private static final Color ALL_COLOR = Color.WHITE;
 
-  private BorderPane root;
+  private static String startLanguage = "English";
+  private static String guiLanguage = "English";
+  private static ResourceBundle myResources = ResourceBundle.getBundle(LANGUAGES_PACKAGE + startLanguage);
+  //private static FileSelect GameFile = new FileSelect(GAME_FILE_EXTENSIONS, GAME_DIRECTORY, myResources.getString("GameFile"), LANGUAGES_PACKAGE + startLanguage);
+
+  private AnchorPane root;
   private Stage myStage;
   private Timeline animation;
+  private GameBoard board;
 
   /**
    * Empty Constructor needed to run the application due to Application requirements
@@ -56,16 +66,22 @@ public class GameGuiController extends Application {
     startAnimationLoop();
     setBorderPane();
     Scene scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
-    //scene.getStylesheets().add(STYLESHEET);
+    scene.getStylesheets().add(STYLESHEET);
     myStage.setScene(scene);
     myStage.show();
   }
 
   private void setBorderPane() {
-    root = new BorderPane();
+    root = new AnchorPane();
     root.setBackground(new Background(new BackgroundFill(ALL_COLOR, CornerRadii.EMPTY, Insets.EMPTY)));
     root.setMaxWidth(SCENE_WIDTH);
     root.setMaxHeight(SCENE_HEIGHT);
+    // MOVE TO METHOD AND CALL IN START
+    board = new GameBoard("connect4board.png");
+    root.setRightAnchor(board, 20.0);
+    root.setTopAnchor(board, 20.0);
+    root.setBottomAnchor(board, 20.0);
+    root.getChildren().addAll(board);
   }
 
   private void startAnimationLoop() {
