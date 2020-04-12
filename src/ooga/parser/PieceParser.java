36e268761a29b0bb662cd56f4142a466f8969cc1
Eric Jiang <edj9@duke.edu>
2020-04-12 10:32:49 -0400

    working on logic to generate piece

--- src/ooga/parser/PieceParser.java
+++ src/ooga/parser/PieceParser.java
@@ -1,8 +1,12 @@
 package ooga.parser;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 import ooga.exceptions.InvalidGridException;
 import ooga.exceptions.InvalidPieceException;
 import ooga.piece.Piece;
+import ooga.piece.movement.Movement;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -22,18 +26,33 @@ public class PieceParser {
    */
   public Piece generatePiece(String pieceSymbol) throws InvalidPieceException {
     String pieceName = pieceSymbol.replaceAll("[\\d]","");
-    String pieceSide = pieceSymbol.replaceAll("[a-zA-Z]","");
-    System.out.println(pieceName + " " + pieceSide);
+    int pieceSide = Integer.parseInt(pieceSymbol.replaceAll("[a-zA-Z]",""));
 
     if (!myPiecesJSON.has(pieceName)) throw new InvalidPieceException("Piece: " + pieceName + " is not defined.");
     JSONObject pieceJSON = myPiecesJSON.getJSONObject(pieceName);
-    for (String pieceField: PIECE_FIELDS) {
-        if (pieceJSON.has(pieceField)){
-          System.out.println("\t"+pieceField + ": " + pieceJSON.get(pieceField));
-        }
+
+    JSONArray normalMovesJSON = pieceJSON.getJSONArray("normalMoves");
+    JSONArray captureMovesJSON = pieceJSON.getJSONArray("captureMoves");
+
+    List<List<Movement>> normalMoves = new ArrayList<List<Movement>>();
+    for (int i = 0; i < normalMovesJSON.length(); i++){
+      JSONArray moveSetJSON = normalMovesJSON.getJSONArray(i);
+      List <Movement> moveSet = new ArrayList<>();
+      for (int j = 0; j < moveSetJSON.length(); j++) {
+        moveSet.append(new Movement());
       }
-    return newPiece;
+      normalMoves.append(moveSet);
+    }
+//    for (String pieceField: PIECE_FIELDS) {
+//        if (pieceJSON.has(pieceField)){
+//          System.out.println("\t"+pieceField + ": " + pieceJSON.get(pieceField));
+//        }
+//    }
+
+    return new Piece(
+        pieceName,
+        pieceSide,
+
+    );
   }
-
-
 }
--- src/ooga/piece/Piece.java
+++ src/ooga/piece/Piece.java
@@ -1,18 +1,25 @@
 package ooga.piece;
 
 import java.util.List;
+import java.util.Map;
 import ooga.piece.movement.Movement;
 
 public class Piece {
-    private List<Movement> normalMovements;
-    private List<Movement> captureMovements;
+    private String name;
+    private int side;
+    private List<List<Movement>> normalMovements;
+    private List<List<Movement>> captureMovements;
+
     private boolean canCapture;
     private boolean canPlace;
     private boolean canJump;
 
-    public Piece(List<Movement> movements, List<Movement> captureMovements, boolean canCapture, boolean canPlace, boolean canJump) {
+    public Piece(String name, int side, List<List<Movement>> movements, List<List<Movement>> captureMovements, boolean canCapture, boolean canPlace, boolean canJump) {
+        this.name = name;
+        this.side = side;
         this.normalMovements = movements;
         this.captureMovements = captureMovements;
+
         this.canCapture = canCapture;
         this.canPlace = canPlace;
         this.canJump = canJump;
