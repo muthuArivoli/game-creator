ef06c0b42bf75f409e59ccdd2a1693615a291c94
Muthu Arivoli <ma381@duke.edu>
2020-04-18 04:30:39 -0400

    add composite movement paths

--- src/ooga/piece/movement/CompositeMovement.java
+++ src/ooga/piece/movement/CompositeMovement.java
@@ -1,7 +1,10 @@
 package ooga.piece.movement;
 
+import ooga.piece.Coordinate;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class CompositeMovement implements Movement {
     private List<Movement> movements;
@@ -18,4 +21,24 @@ public class CompositeMovement implements Movement {
         return this.movements;
     }
 
+
+    @Override
+    public List<List<Coordinate>> getValidPaths(Coordinate position, int playerSide, Predicate<Coordinate> checkCoordinatesInBound) {
+        List<List<Coordinate>> paths = new ArrayList<>();
+        paths.add(new ArrayList<>());
+        paths.addAll(movements.get(0).getValidPaths(position,playerSide,checkCoordinatesInBound));
+        for(int i=1;i<movements.size();i++){
+            List<List<Coordinate>> newPaths = new ArrayList<>();
+            for(int k=0;k<paths.size();k++){
+                //Coordinate c is the last value in each path (each coordinate list in paths)
+                List<List<Coordinate>> pathsToAdd = movements.get(i).getValidPaths(paths.get(k).get(paths.size()-1),playerSide,checkCoordinatesInBound);
+                for(int j=0;j<pathsToAdd.size();j++){
+                    newPaths.add(new ArrayList<>(paths.get(k)));
+                    newPaths.get(newPaths.size()-1).addAll(pathsToAdd.get(j));
+                }
+            }
+            paths = newPaths;
+        }
+        return paths;
+    }
 }
