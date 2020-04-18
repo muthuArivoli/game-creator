7ce3a6fc32511210ac249a8d0893bef4e8d9960f
Muthu Arivoli <ma381@duke.edu>
2020-04-18 04:26:47 -0400

    implement bound checking for movements

--- src/ooga/piece/movement/diagFdLeft.java
+++ src/ooga/piece/movement/diagFdLeft.java
@@ -18,11 +18,19 @@ public class diagFdLeft implements Movement {
   @Override
   public List<List<Coordinate>> getValidPaths(Coordinate position, int playerSide, Predicate<Coordinate> checkCoordinatesInBound) {
     List<List<Coordinate>> paths = new ArrayList<>();
+  Coordinate nextCoord = new Coordinate(position.getRow()-rangeBegin*playerSide,position.getCol()+rangeBegin*playerSide);
+    if(!checkCoordinatesInBound.test(nextCoord)){
+      return paths;
+    }
     paths.add(new ArrayList<>());
-    paths.get(0).add(new Coordinate(position.getRow()-rangeBegin*playerSide,position.getCol()+rangeBegin*playerSide));
+    paths.get(0).add(nextCoord);
     for(int i=rangeBegin+1;i<=rangeEnd;i++){
+      nextCoord = new Coordinate(position.getRow()-i*playerSide,position.getCol()+i*playerSide);
+      if(!checkCoordinatesInBound.test(nextCoord)){
+        return paths;
+      }
       paths.add(new ArrayList<>(paths.get(paths.size()-1)));
-      paths.get(paths.size()-1).add(new Coordinate(position.getRow()-i*playerSide,position.getCol()+i*playerSide));
+      paths.get(paths.size()-1).add(nextCoord);
     }
     return paths;
   }
--- src/ooga/piece/movement/left.java
+++ src/ooga/piece/movement/left.java
@@ -18,11 +18,19 @@ public class left implements Movement {
     @Override
     public List<List<Coordinate>> getValidPaths(Coordinate position, int playerSide, Predicate<Coordinate> checkCoordinatesInBound) {
         List<List<Coordinate>> paths = new ArrayList<>();
+        Coordinate nextCoord = new Coordinate(position.getRow()-rangeBegin*playerSide,position.getCol());
+        if(!checkCoordinatesInBound.test(nextCoord)){
+            return paths;
+        }
         paths.add(new ArrayList<>());
-        paths.get(0).add(new Coordinate(position.getRow()-rangeBegin*playerSide,position.getCol()));
+        paths.get(0).add(nextCoord);
         for(int i=rangeBegin+1;i<=rangeEnd;i++){
+            nextCoord = new Coordinate(position.getRow()-i*playerSide,position.getCol());
+            if(!checkCoordinatesInBound.test(nextCoord)){
+                return paths;
+            }
             paths.add(new ArrayList<>(paths.get(paths.size()-1)));
-            paths.get(paths.size()-1).add(new Coordinate(position.getRow()-i*playerSide,position.getCol()));
+            paths.get(paths.size()-1).add(nextCoord);
         }
         return paths;
     }
