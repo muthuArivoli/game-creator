346e1010ccd515a8f225bb4b65dc7eaa659bc0c0
Alexander Uzochukwu <aou@duke.edu>
2020-04-19 08:01:20 -0400

    Created Abstract Piece Class

---
+++ src/ooga/game_view/board/pieceType/PieceShape.java
@@ -0,0 +1,9 @@
+package ooga.game_view.board.pieceType;
+
+import javafx.scene.layout.StackPane;
+
+public class PieceShape extends StackPane {
+    public PieceShape(final String t) {
+      
+    }
+}
