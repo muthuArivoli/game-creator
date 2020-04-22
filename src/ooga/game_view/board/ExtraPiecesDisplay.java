0e16e9810e7af7cf5cc29d589ebadce52dce1804
Alexander Uzochukwu <aou@duke.edu>
2020-04-22 15:46:25 -0400

    Completed move of pieces display to new class

--- src/ooga/game_view/board/ExtraPiecesDisplay.java
+++ src/ooga/game_view/board/ExtraPiecesDisplay.java
@@ -1,17 +1,32 @@
 package ooga.game_view.board;
 
+import java.awt.List;
+import java.util.ArrayList;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import ooga.controller.GameController;
 
 public class ExtraPiecesDisplay extends HBox {
+  private GameController gameController;
+  private ArrayList<String> pieceList;
 
-  public ExtraPiecesDisplay(double width, double height){
+  public ExtraPiecesDisplay(double width, double height, GameController gameController){
+    this.gameController = gameController;
     this.setPrefSize(width, height);
     this.getStyleClass().add("displayBox");
     this.setAlignment(Pos.CENTER);
 
+    pieceList = new ArrayList<>();
     addMainButton(width, height);
+  }
+
+  public void addPieces(String pieceName){
+    pieceList.add(pieceName);
   }
 
   private void addMainButton(double width, double height){
@@ -22,5 +37,17 @@ public class ExtraPiecesDisplay extends HBox {
   }
 
   private void displayAvailablePieces() {
+    VBox rt = new VBox();
+    rt.setAlignment(Pos.CENTER);
+    for (String name: pieceList){
+      Button piece = new Button(name);
+      //piece.setOnAction(e -> gameController.handleClick(name));
+      rt.getChildren().addAll(piece);
+    }
+    Stage s = new Stage();
+    s.setTitle("CHOOSE EXTRA PIECE");
+    Scene temporaryScene = new Scene(rt, 250,250);
+    s.setScene(temporaryScene);
+    s.show();
   }
 }
