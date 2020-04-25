5c8e881dd66bf57514bea4898a52da7718fe3d99
Eric Jiang <edj9@duke.edu>
2020-04-24 23:07:49 -0400

    added junit tests for controller

--- src/ooga/tests/APITests/GameControllerTest.java
+++ src/ooga/tests/APITests/GameControllerTest.java
@@ -1,5 +1,7 @@
-package ooga.tests.UITests;
+package ooga.tests.APITests;
 
+import static org.hamcrest.CoreMatchers.anyOf;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.hasItems;
 import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,6 +19,7 @@ import ooga.game_view.board.GameBoard;
 import ooga.game_view.board.availableShapes.ComponentShape;
 import ooga.goals.Goal;
 import ooga.goals.enemyPiecesLeft;
+import ooga.models.GameModel;
 import ooga.models.GridModel;
 import ooga.piece.Coordinate;
 import ooga.piece.Piece;
@@ -41,14 +44,40 @@ class GameControllerTest {
   }
 
   @Test
-  void testNoMovementClicks () {
-    myGameController.handleClick(1,1);
-    myGameController.handleClick(2,4);
-    myGameController.handleClick(8,8);
+  void invalidClicks () {
+    myGameController.handleClick(7,5);
+    myGameController.handleClick(6,4);
+    myGameController.handleClick(0,8);
+    myGameController.handleClick(308,308);
     assertNull(myGameController.getSelectedPiecePosition());
   }
 
-  
+  @Test
+  void testValidGoals () {
+    GameModel myGameModel = myGameController.getGameModel();
+    List<Goal> myGoals = myGameModel.getGoals();
+    for (Goal g: myGoals) {
+      assertThat(g.getClass().toString(), anyOf(containsString("enemyPiecesLeft"), containsString("pieceTaken")));
+    }
+  }
+  @Test
+  void checkGameWinner() {
+    // after capturing the player 2 king with the queen, assert that player 1 is declared winner
+    myGameController.toggleAI();
+    myGameController.handleClick(6, 2);
+    myGameController.handleClick(4, 2);
+    myGameController.handleClick(1,3);
+    myGameController.handleClick(3,3);
+    myGameController.handleClick(7,3);
+    myGameController.handleClick(4,0);
+    myGameController.handleClick(1,7);
+    myGameController.handleClick(2,7);
+    myGameController.handleClick(4,0);
+    myGameController.handleClick(0,4);
+
+    assertEquals(myGameController.checkGameEnd(), 1);
+  }
+
 
   @org.junit.jupiter.api.AfterEach
   void tearDown() {
